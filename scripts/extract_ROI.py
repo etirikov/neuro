f3e2e8a0dd4a56c395163ac27a2fcf0b09b6b4eb
root <root@s4.lab23.ipiran.ru>
2020-04-04 21:28:31 +0300

    working version

--- scripts/extract_ROI.py
+++ scripts/extract_ROI.py
@@ -1,20 +1,35 @@
-import nibabel
 import os
 import sys
 import glob
 import configparser
+import subprocess
+import socket
+from multiprocessing.pool import Pool
+import shutil
+
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+
 from nilearn.input_data import NiftiLabelsMasker
 from nilearn import image
 from nilearn.connectome import ConnectivityMeasure
 from nilearn import datasets
-import seaborn as sns
-from multiprocessing.pool import Pool
+import nibabel
+
 from scipy.signal import savgol_filter, medfilt
-import subprocess
-import redis
+
+from pyspark.sql.functions import col, pandas_udf, PandasUDFType
+from pyspark.sql.types import NullType
+from pyspark.sql import SparkSession
+from pyspark import SparkConf
+conf = SparkConf()
+# conf.setMaster('yarn-client')
+conf.setAppName('Test')
+conf.set("spark.hadoop.yarn.resourcemanager.hostname", '172.17.14.17')
+conf.set("spark.hadoop.yarn.resourcemanager.address", '172.17.14.17:8050')
+conf.set('spark.executor.memoryOverhead', '10GB')
+conf.set('spark.executor.instances', '12')
+spark = SparkSession.builder.config(conf=conf).getOrCreate()
 
 def get_ROI(path, masker, delete_confounds):
     if delete_confounds:
@@ -24,15 +39,14 @@ def get_ROI(path, masker, delete_confounds):
         return masker.transform(path)
     
 
-def get_masker(atlas_name):
+def get_masker(atlas_name, atlas_path):
     if atlas_name=='harvard_oxford':
-        atlas = datasets.fetch_atlas_harvard_oxford('cort-maxprob-thr25-2mm')
+        atlas = datasets.fetch_atlas_harvard_oxford('cort-maxprob-thr25-2mm', data_dir=atlas_path)
     else:
         raise OSError('there is no atlas {0}'.format(atlas_name))
     atlas_filename = atlas.maps
 
     masker = NiftiLabelsMasker(labels_img=atlas_filename, 
-                               standardize=True,
                                memory='nilearn_cache')
     masker.fit()
     return masker
@@ -46,15 +60,9 @@ def parallel_jobs_one_machine(config, person_path, person_number, masker, remove
     if config['ATLAS']['where_to_write']=='hdd':
         data.to_csv(save_path, index=False)
     else:
-        redis_db = redis.Redis(host=config['DATABASE']['host'], port=config['DATABASE']['port'])
-        name = 'dataset_{}_people_{}_atlas_{}'.format(config['DATASET']['name'],
-                                                      person_number,
-                                                      config['ATLAS']['name'])
-        redis_db.sadd(name, data.to_json())
+        pass
         
         
-
-
 def extract_ROI_one_machine(config_path):
     config = configparser.ConfigParser()
     config.read(config_path)
@@ -94,8 +102,46 @@ def extract_ROI_one_machine(config_path):
         with Pool(n_jobs) as pool:
             pool.starmap(parallel_jobs_one_machine, params)
 
-def extract_ROI_spark(config_path):
-    pass
+
+@pandas_udf("path string, people_number long", PandasUDFType.GROUPED_MAP)
+def extract_ROI_spark(data):
+    print('111', data.shape)
+    people_path = data['path']
+    people_number = np.unique(data['people_number'])[0]
+
+    atlas_path = '/data/test/nilearn_data'
+    
+    masker = get_masker(atlas_name, atlas_path)
+    j = 0
+    for person in zip(people_path, [people_number]*data.shape[1]):
+        person_path = person[0]
+        person_number = person[1]
+        print(person_path, person_number)
+        data_person = get_ROI(person_path, masker, remove_confounds)
+        columns = ['x' + str(i) for i in range(data_person.shape[1])]
+        data_person = pd.DataFrame(data_person, columns=columns)
+        save_path = config['ATLAS']['path_to_write'] + str(person_number)+'_{}_.csv'.format(j)
+        j+=1
+        if config['ATLAS']['where_to_write']=='hdd':
+            data_person.to_csv(save_path, index=False)
+
+    temp = pd.DataFrame([[j,people_number]], columns=['path', 'people_number'])
+    return temp
 
 if __name__ == '__main__':
-    extract_ROI_one_machine('config.ini')
+    config_path = './config.ini'
+    config = configparser.ConfigParser()
+    config.read(config_path)
+
+    data_path = config['DATA']['data_path']
+    atlas_name = config['ATLAS']['name']
+    remove_confounds = bool(config['ATLAS']['remove_confounds'])
+    n_jobs = int(config['GENERAL']['n_jobs'])
+
+    masker = get_masker(atlas_name, None)
+    
+    data = pd.read_csv(data_path)
+    data_spark = spark.createDataFrame(data)
+
+    df1 = data_spark.groupby("people_number").apply(extract_ROI_spark)
+    print(df1.select('path').toPandas())
